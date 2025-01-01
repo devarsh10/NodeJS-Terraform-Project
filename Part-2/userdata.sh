e5f4719cb75e0eeb12e192bdf7776789c503e1c5
Devarsh <devarshshah2608@gmail.com>
2025-01-01 14:23:16 +0530

    rollback to previous version

--- Part-2/userdata.sh
+++ Part-2/userdata.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y git
+sudo apt update -y
+sudo apt install -y curl unzip git
 
-# curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-# sudo apt install -y nodejs
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.bashrc
-nvm install --lts
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
 
 echo -e ".
 .
 .
 .
-node -v => $(node -v)
-npm -v => $(npm -v)
+Installed nodejs
 .
 .
 .
 ."
 
 sudo apt install net-tools -y
-
-echo -e ".
-.
-.
-net-tools installed
-.
-.
-."
 
 git clone https://devarsh10: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@github.com/devarsh10/http-code.git /home/ubuntu/app
 cd /home/ubuntu/app/Part-1
@@ -43,7 +31,7 @@ npm install done
 .
 ."
 
-cat <<EOT > .env #overriding with existing .env
+cat <<EOT >> .env #overriding with existing .env
 AWS_ACCESS_KEY_ID=process.env.AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY=process.env.AWS_SECRET_ACCESS_KEY
 AWS_REGION=us-east-1
