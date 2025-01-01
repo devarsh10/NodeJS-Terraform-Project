3b4ad532560ec86c20830a3d2f0e2dabb53a58df
Devarsh <devarshshah2608@gmail.com>
2025-01-01 13:02:36 +0530

    add path in nodejs file

--- Part-2/userdata.sh
+++ Part-2/userdata.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y curl unzip git
+sudo apt update
+sudo apt install -y git
 
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+# sudo apt install -y nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source ~/.bashrc
+nvm install --lts
 
 echo -e ".
 .
 .
 .
-Installed nodejs
+node -v => $(node -v)
+npm -v => $(npm -v)
 .
 .
 .
 ."
 
 sudo apt install net-tools -y
+
+echo -e ".
+.
+.
+net-tools installed
+.
+.
+."
 
 git clone https://devarsh10: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@github.com/devarsh10/http-code.git /home/ubuntu/app
 cd /home/ubuntu/app/Part-1
@@ -31,12 +43,12 @@ npm install done
 .
 ."
 
-cat <<EOT >> .env #overriding with existing .env
-PORT=80
-AWS_ACCESS_KEY_ID=AKIASZGKPSJIWXEXIATM
-AWS_SECRET_ACCESS_KEY=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
+cat <<EOT > .env #overriding with existing .env
+AWS_ACCESS_KEY_ID=process.env.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=process.env.AWS_SECRET_ACCESS_KEY
 AWS_REGION=us-east-1
 BUCKET_NAME=list-bucket-content
+PORT=3000
 EOT
 
 echo -e ".
